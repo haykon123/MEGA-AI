0c06007b2963642bae44d053ec55449b58c758da
haykon123 <adekunle12.adeyemi@gmail.com>
2025-01-24 03:34:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '23429359790';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '2348029369790' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -25,7 +25,7 @@ global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
 
 global.mods = []
 global.prems = []
-global.allowed = ['923444844060', '923051391007']
+global.allowed = ['923444844060','2348029359790']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
