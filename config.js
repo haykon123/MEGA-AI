a30377b59702e7045685924ac4c5edb5f129e479
haykon123 <adekunle12.adeyemi@gmail.com>
2025-03-12 12:18:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '2348029359790' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'ND7557KJ' ;  // put your session id here
 
 global.mods = []
 global.prems = []
